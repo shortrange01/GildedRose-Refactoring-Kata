4ab73d1a3d00611fe580293185162312de29ec3d
shortrange01 <shortrange01@gmail.com>
2021-07-03 21:32:08 +0900

    refactor: 読みづらい条件分岐を簡略化

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -18,58 +18,49 @@ export class GildedRose {
     }
 
     updateQuality(): Item[] {
-        for (let i = 0; i < this.items.length; i++) {
-            if (
-                this.items[i].name != 'Aged Brie' &&
-                this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-            ) {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality =
-                            this.items[i].name != 'Conjured' ? this.items[i].quality - 1 : this.items[i].quality - 2;
+        this.items.forEach((item: Item) => {
+            // Sulfurasは何も変更せずに早期リターン
+            if (item.name === 'Sulfuras, Hand of Ragnaros') return;
+
+            if (item.name === 'Aged Brie' || item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                    if (item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.quality < 50) {
+                        if (item.sellIn < 11) item.quality = item.quality + 1;
+                        if (item.sellIn < 6 && item.quality < 50) item.quality = item.quality + 1;
                     }
                 }
             } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1;
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1;
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1;
-                            }
-                        }
-                    }
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                    if (item.name === 'Conjured' && item.quality > 0) item.quality = item.quality - 1;
                 }
             }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
+
+            // 販売期間を1減少させる
+            item.sellIn = item.sellIn - 1;
+
+            if (item.sellIn >= 0) return;
+
+            // [begin] 販売期限を過ぎた場合は、Quality 値をさらに増減させる
+            if (item.name === 'Aged Brie') {
+                if (item.quality >= 50) return;
+                item.quality = item.quality + 1;
+                return;
             }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality =
-                                    this.items[i].name != 'Conjured'
-                                        ? this.items[i].quality - 1
-                                        : this.items[i].quality - 2;
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality;
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1;
-                    }
-                }
+
+            // Backstage passes は販売期間が過ぎると0になる
+            if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+                item.quality = 0;
+                return;
             }
-        }
+
+            if (item.quality <= 0) return;
+
+            item.quality = item.quality - 1;
+            if (item.name === 'Conjured' && item.quality > 0) item.quality = item.quality - 1;
+            // [end] 販売期限を過ぎた場合は、Quality 値をさらに減少させる
+        });
 
         return this.items;
     }

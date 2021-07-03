7f33e9ca3d73d82381fe623574639796d9947da5
shortrange01 <shortrange01@gmail.com>
2021-07-03 17:19:41 +0900

    test: テスト項目追加

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -2,11 +2,24 @@ import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
-
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+    it('Gilded Rose にアイテムを追加できる', function () {
+        const gildedRose = new GildedRose([new Item('foo', 10, 20)]);
+        expect(gildedRose.items[0].name).to.equal('foo');
+        expect(gildedRose.items[0].sellIn).to.equal(10);
+        expect(gildedRose.items[0].quality).to.equal(20);
     });
 
+    it('Gilded Rose にアイテムを追加して日をまたぐとsellInとqualityが1減少する', function () {
+        const gildedRose = new GildedRose([new Item('foo', 10, 20)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].name).to.equal('foo');
+        expect(items[0].sellIn).to.equal(9);
+        expect(items[0].quality).to.equal(19);
+    });
+
+    it('qualityは負数にならない', function () {
+        const gildedRose = new GildedRose([new Item('foo', 10, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(0);
+    });
 });

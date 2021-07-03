34bd986547b6511ef1893f70ee8ecb890487f7b7
shortrange01 <shortrange01@gmail.com>
2021-07-03 21:31:43 +0900

    test: sellInのテスト追記

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -23,18 +23,27 @@ describe('Gilded Rose', () => {
         expect(items[0].quality).to.equal(0);
     });
 
-    it('Sulfurasは常に Quality 値 80', () => {
-        const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 1, 80)]);
-        gildedRose.updateQuality();
-        const item = gildedRose.updateQuality();
-        expect(item[0].quality).to.equal(80);
+    describe('Sulfuras', () => {
+        it('Quality 値は常に 80', () => {
+            const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 1, 80)]);
+            gildedRose.updateQuality();
+            const item = gildedRose.updateQuality();
+            expect(item[0].quality).to.equal(80);
+        });
+        it('SellIn は変化しない', () => {
+            const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 3, 80)]);
+            gildedRose.updateQuality();
+            const item = gildedRose.updateQuality();
+            expect(item[0].sellIn).to.equal(3);
+        });
     });
 
     describe('Aged Brie', () => {
-        it('日が経つほど Quality 値が上がる', () => {
+        it('日が経つほど Quality 値が上がる, SellIn は下がる', () => {
             const gildedRose = new GildedRose([new Item('Aged Brie', 3, 10)]);
             const item = gildedRose.updateQuality();
             expect(item[0].quality).to.equal(11);
+            expect(item[0].sellIn).to.equal(2);
         });
 
         it('Quality 値は 50 がmax', () => {
@@ -49,32 +58,36 @@ describe('Gilded Rose', () => {
     });
 
     describe('Backstage passes', () => {
-        it('SellIn が 10 を越える場合 Quality 値が 1 上がる', () => {
+        it('SellIn が 10 を越える場合 Quality 値が 1 上がる, SellIn は下がる', () => {
             const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 20, 10)]);
             const item = gildedRose.updateQuality();
             expect(item[0].quality).to.equal(11);
         });
 
-        it('SellIn が 10 以内の場合 Quality 値が 2 上がる', () => {
+        it('SellIn が 10 以内の場合 Quality 値が 2 上がる, SellIn は下がる', () => {
             const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10)]);
             const item = gildedRose.updateQuality();
             expect(item[0].quality).to.equal(12);
+            expect(item[0].sellIn).to.equal(9);
 
             const gildedRose2 = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 6, 10)]);
             const item2 = gildedRose2.updateQuality();
             expect(item2[0].quality).to.equal(12);
+            expect(item2[0].sellIn).to.equal(5);
         });
 
-        it('SellIn が 5 以内の場合 Quality 値が 3 上がる', () => {
+        it('SellIn が 5 以内の場合 Quality 値が 3 上がる, SellIn は下がる', () => {
             const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 10)]);
             const item = gildedRose.updateQuality();
             expect(item[0].quality).to.equal(13);
+            expect(item[0].sellIn).to.equal(4);
         });
 
-        it('SellIn が マイナスの場合 Quality 値は 0 になる', () => {
+        it('SellIn が マイナスの場合 Quality 値は 0 になる, SellIn は下がる', () => {
             const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', -1, 10)]);
             const item = gildedRose.updateQuality();
             expect(item[0].quality).to.equal(0);
+            expect(item[0].sellIn).to.equal(-2);
         });
     });
 
@@ -87,6 +100,7 @@ describe('Gilded Rose', () => {
             }
             const item = gildedRose.updateQuality();
             expect(item[0].quality).to.equal(6);
+            expect(item[0].sellIn).to.equal(-3);
         });
     });
 });

a30b486926c18ddb6675a8da2826ecef37613545
shortrange01 <shortrange01@gmail.com>
2021-07-03 20:30:57 +0900

    feat: サブプライヤーConjuredの条件追加

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -25,7 +25,8 @@ export class GildedRose {
             ) {
                 if (this.items[i].quality > 0) {
                     if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1;
+                        this.items[i].quality =
+                            this.items[i].name != 'Conjured' ? this.items[i].quality - 1 : this.items[i].quality - 2;
                     }
                 }
             } else {
@@ -53,7 +54,10 @@ export class GildedRose {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if (this.items[i].quality > 0) {
                             if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1;
+                                this.items[i].quality =
+                                    this.items[i].name != 'Conjured'
+                                        ? this.items[i].quality - 1
+                                        : this.items[i].quality - 2;
                             }
                         }
                     } else {

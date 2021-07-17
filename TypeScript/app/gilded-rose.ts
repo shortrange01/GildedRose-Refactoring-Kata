6ac4b879db2fcb052dc2d601cdfd61a893b2dadd
shortrange01 <shortrange01@gmail.com>
2021-07-18 03:24:33 +0900

    feat: 関数分割

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -22,46 +22,64 @@ export class GildedRose {
             // Sulfurasは何も変更せずに早期リターン
             if (item.name === 'Sulfuras, Hand of Ragnaros') return;
 
-            if (item.name === 'Aged Brie' || item.name === 'Backstage passes to a TAFKAL80ETC concert') {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                    if (item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.quality < 50) {
-                        if (item.sellIn < 11) item.quality = item.quality + 1;
-                        if (item.sellIn < 6 && item.quality < 50) item.quality = item.quality + 1;
-                    }
-                }
-            } else {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                    if (item.name === 'Conjured' && item.quality > 0) item.quality = item.quality - 1;
-                }
-            }
-
-            // 販売期間を1減少させる
-            item.sellIn = item.sellIn - 1;
-
-            if (item.sellIn >= 0) return;
-
-            // [begin] 販売期限を過ぎた場合は、Quality 値をさらに増減させる
             if (item.name === 'Aged Brie') {
-                if (item.quality >= 50) return;
-                item.quality = item.quality + 1;
+                item = this.updateAgedBrie(item);
                 return;
             }
 
-            // Backstage passes は販売期間が過ぎると0になる
             if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
-                item.quality = 0;
+                item = this.updateBackstagePasses(item);
+                return;
+            }
+            
+            if(item.quality <= 0) {
+                item.sellIn = item.sellIn - 1;
                 return;
             }
 
-            if (item.quality <= 0) return;
-
-            item.quality = item.quality - 1;
-            if (item.name === 'Conjured' && item.quality > 0) item.quality = item.quality - 1;
-            // [end] 販売期限を過ぎた場合は、Quality 値をさらに減少させる
+            item = this.updateOtherItem(item);
         });
 
         return this.items;
+    }
+
+    updateAgedBrie(item: Item): Item {
+        item.quality = item.quality < 50 ? item.quality + 1 : item.quality;
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn >= 0) return item;
+        item.quality = item.quality < 50 ? item.quality + 1 : item.quality;
+        return item;
+    }
+
+    updateBackstagePasses(item: Item): Item {
+        item.quality = item.quality < 50 ? item.quality + 1 : item.quality;
+        if (item.quality < 50) {
+            if (item.sellIn < 11) item.quality = item.quality + 1;
+            if (item.sellIn < 6 && item.quality < 50) item.quality = item.quality + 1;
+        }
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn >= 0) return item;
+        // Backstage passes は販売期間が過ぎると0になる
+        item.quality = 0;
+        return item;
+    }
+
+    updateOtherItem(item: Item): Item {
+            
+        item.quality = item.quality - 1;
+        if (item.name === 'Conjured' && item.quality > 0) item.quality = item.quality - 1;
+        
+        // 販売期間を1減少させる
+        item.sellIn = item.sellIn - 1;
+
+        // 販売期間中またはqualityが0の場合は処理終了
+        if (item.sellIn >= 0 || item.quality <= 0) return item;
+
+        // 販売期限を過ぎた場合は、Quality 値をさらに増減させる
+        item.quality = item.quality - 1;
+        if (item.name === 'Conjured' && item.quality > 0) item.quality = item.quality - 1;
+
+        return item;
+
     }
 }
